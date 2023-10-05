31d8f50adfec096ca594036cf1ccd9e94b762ed7
IceK37 <45316097+IceK37@users.noreply.github.com>
2023-10-05 13:58:50 +0200

    com + dev

---
+++ php_template/communication.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+include "header.php";
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pole Communication</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container-home-communication">
+
+    </div>
+    <footer>
+        <p>&copy; 2023 YPN. Tous droits réservés.</p>
+    </footer>
+</body>
+</html>
--- php_template/header.php
+++ php_template/header.php
@@ -12,6 +12,8 @@
         <div class="container">
             <h1>YPN Portal</h1>
             <ul>
+                <li><a href="../php_template/dev.php">Pôle Dev</a></li>
+                <li><a href="../php_template/Communication.php">Pôle Communication</a></li>
                 <li><a href="../php_template/index.php">Accueil</a></li>
                 <li><a href="../php_template/propos.php">À propos</a></li>
             </ul>
