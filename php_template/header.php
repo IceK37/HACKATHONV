e944d90e4dfdea9bd27196bed1b1bdde1c7393de
IceK37 <45316097+IceK37@users.noreply.github.com>
2023-10-05 09:19:33 +0200

    fix repo

---
+++ php_template/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil VPN</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+    <nav>
+        <div class="container">
+            <h1>YPN Portal</h1>
+            <ul>
+                <li><a href="../php_template/index.php">Accueil</a></li>
+                <li><a href="../php_template/propos.php">À propos</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
