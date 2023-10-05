b297c72a0d0ccf537d4eadd0c6b601175b4fce9d
IceK37 <45316097+IceK37@users.noreply.github.com>
2023-10-05 14:26:10 +0200

    Verif session navbar

--- php_template/header.php
+++ php_template/header.php
@@ -12,8 +12,24 @@
         <div class="container">
             <h1>YPN Portal</h1>
             <ul>
-                <li><a href="../php_template/dev.php">Pôle Dev</a></li>
-                <li><a href="../php_template/Communication.php">Pôle Communication</a></li>
+                <?php
+                session_start();
+                // Vérifiez si l'utilisateur est connecté
+                if (isset($_SESSION['username'])) {
+                    // Récupérez le rôle de l'utilisateur depuis la variable de session 'role' s'il est connecté
+                    if (isset($_SESSION['role'])) {
+                        $userRole = $_SESSION['role'];
+                        // Affichez le bouton "Pôle Dev" si l'utilisateur a le rôle approprié
+                        if ($userRole == 'dev') {
+                            echo '<li><a href="../php_template/dev.php">Pôle Dev</a></li>';
+                        }
+                        // Affichez le bouton "Pôle Communication" si l'utilisateur a le rôle approprié
+                        if ($userRole == 'communication') {
+                            echo '<li><a href="../php_template/Communication.php">Pôle Communication</a></li>';
+                        }
+                    }
+                }
+                ?>
                 <li><a href="../php_template/index.php">Accueil</a></li>
                 <li><a href="../php_template/propos.php">À propos</a></li>
             </ul>
